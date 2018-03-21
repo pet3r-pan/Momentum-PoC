dd325b7a71a030ed14500170d532a116b9c9c4ef
Regio Michelin <regio@vulture.local>
2018-03-21 12:48:37 +1100

    removing useless parameter

--- user.py
+++ user.py
@@ -63,7 +63,7 @@ def main():
 
 if __name__ == '__main__':
 
-    if len(sys.argv[1:]) < 1:
+    if len(sys.argv[1:]) < 0:
         print ("Command Line usage:")
         print ("    python user.py TBD")
         quit()
